6738b6c0045b63111af781334c479d91c16ecd72
John <tmactodarack@gmail.com>
2024-02-27 23:56:29 -0600

    'ss'

--- pages/CPI.py
+++ pages/CPI.py
@@ -6,6 +6,7 @@ from datetime import timedelta
 import plotly.express as px
 import plotly.graph_objects as go
 import plotly.io as pio
+from plotly.subplots import make_subplots
 
 ################################ Fetch data ######################################################
 # API key for FRED 
@@ -15,7 +16,14 @@ tickers = {'CPIAUCSL': 'CPI', #all cpi related are SA
            'CPIUFDSL': 'CPI Food', 
            'CPIENGSL': 'CPI Energy', 
            'PCEPI': 'PCE', 
-           'PCEPILFE': 'Core PCE'
+           'PCEPILFE': 'Core PCE',
+           'CUSR0000SACL1E': 'CPI Core Goods',
+           'CUSR0000SASLE': 'CPI Core Service',
+           'CUSR0000SAH1': 'Shelter',
+           'CUSR0000SAM2': 'Medical Care', 
+           'CUSR0000SAS4': 'Transportation',
+           'CPIEDUSL':'Education and Communication', 
+           'CPIRECSL':'Recreation'
            } 
 
 @ st.cache_data
@@ -58,7 +66,8 @@ with tab1:
                       showlegend=True,
                       title=dict(text=title, font=dict(size=25)),
                       margin=dict(b=0,t=80,r=0,l=0),
-                      legend = dict(orientation='h',x=1, y=-0.15, xanchor='right', yanchor='top'),
+                      legend = dict(orientation='h',x=1, y=-0.15, xanchor='right', yanchor='top'), 
+                      # legend = dict(x=0.01, y=0.99, xanchor='left', yanchor='top'),
                       xaxis=dict(fixedrange=True), 
                       yaxis=dict(tickformat='.1%', fixedrange=True))
     return fig, data
@@ -67,8 +76,8 @@ with tab1:
     st.session_state['cpi_yoy_fig_period'] = 'Past 2 Years'
   ##### Trigger function below #####
   fig, data = cpi_yoy( dt.today()-timedelta(days=365*float(st.session_state['cpi_yoy_fig_period'][5])), dt.today() )
-  st.radio('Display Period', ['Past 1 Year', 'Past 2 Years', 'Past 3 Years', 'Past 5 Years'], key='cpi_yoy_fig_period', horizontal=True, label_visibility='collapsed')  
   st.plotly_chart(fig, use_container_width=True)
+  st.radio('Display Period', ['Past 1 Year', 'Past 2 Years', 'Past 3 Years', 'Past 5 Years'], key='cpi_yoy_fig_period', horizontal=True)  
   # st.write(fig.data[0]['y'])
 with tab2:
 ###################################### CPI YoY + 6m annualized ######################################
@@ -97,7 +106,211 @@ with tab2:
     st.session_state['cpi_6m_fig_period'] = 'Past 2 Years'
   ##### Trigger function below #####
   fig, data = cpi_6m( dt.today()-timedelta(days=365*float(st.session_state['cpi_6m_fig_period'][5])), dt.today() )
-  st.radio('Display Period', ['Past 1 Year', 'Past 2 Years', 'Past 3 Years', 'Past 5 Years'], key='cpi_6m_fig_period', horizontal=True, label_visibility='collapsed')
   st.plotly_chart(fig, use_container_width=True)
+  st.radio('Display Period', ['Past 1 Year', 'Past 2 Years', 'Past 3 Years', 'Past 5 Years'], key='cpi_6m_fig_period', horizontal=True) # label_visibility='collapsed'
 
 
+
+st.markdown('***')
+###################################### CPI MoM ######################################
+def cpi_mom(start, end):
+  data = combine[['CPI', 'Core CPI', 'CPI Food', 'CPI Energy']].pct_change().dropna().loc[start:end]
+  update = updates['CPI']
+  title = 'CPI MoM Breakdown'
+
+  fig = make_subplots(rows=2, cols=2, subplot_titles=("Headline CPI MoM", "Core CPI MoM", "CPI Food MoM", "CPI Energy MoM"))
+  fig.add_trace(go.Bar(x=data.index, y=data['CPI'], name='CPI MoM'), row=1, col=1)
+  fig.add_trace(go.Bar(x=data.index, y=data['Core CPI'], name='Core CPI MoM'), row=1, col=2)
+  fig.add_trace(go.Bar(x=data.index, y=data['CPI Food'], name='CPI Food MoM'), row=2, col=1)
+  fig.add_trace(go.Bar(x=data.index, y=data['CPI'], name='CPI Energy MoM'), row=2, col=2)
+  # fig.add_trace(go.Scatter(x=[data.index[-1]], y=[fig.data[0]['y'][-1]-0.002], mode='markers', marker_symbol='triangle-up', marker_color='red', name='Latest Release: ' + f"{update:%m/%d/%y}" + f" ({data.index[-1]:%b})"))
+  # fig.add_vline(x=data.index[-1], line_width=3, line_dash="dash", line_color="green")
+  fig.update_layout(template='seaborn', 
+                    title=dict(text=title, font=dict(size=25)),
+                    margin=dict(b=0,t=80,r=0,l=0),
+                    legend = dict(orientation='h',x=1, y=-0.15, xanchor='right', yanchor='top'))
+
+  fig.update_yaxes(tickformat='.1%',fixedrange=True)
+  fig.update_xaxes(fixedrange=True)
+  
+  return fig, data
+
+if 'cpi_mom_fig_period' not in st.session_state: 
+  st.session_state['cpi_mom_fig_period'] = 'Past 2 Years'
+##### Trigger function below #####
+fig, data = cpi_mom( dt.today()-timedelta(days=365*float(st.session_state['cpi_mom_fig_period'][5])), dt.today() )
+st.plotly_chart(fig, use_container_width=True)
+st.radio('Display Period', ['Past 1 Year', 'Past 2 Years', 'Past 3 Years', 'Past 5 Years'], key='cpi_mom_fig_period', horizontal=True) # label_visibility='collapsed'  
+# st.write(fig.data[0]['y'])
+
+
+
+st.markdown('***')
+tab1, tab2 = st.tabs(['Core Breakdown YoY', 'Core Breakdown MoM'])
+with tab1:
+###################################### CPI YoY ######################################
+  def core_yoy(start, end):
+    data = combine[['CPI Core Goods', 'CPI Core Service']].pct_change(12).dropna().loc[start:end]
+    update = updates['CPI']
+    title = 'Core Goods vs Core Service YoY'
+
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=data.index, y=data['CPI Core Goods'], name='Core Goods YoY', mode='lines+markers'))
+    fig.add_trace(go.Scatter(x=data.index, y=data['CPI Core Service'], name='Core Service YoY', mode='lines+markers'))
+    fig.add_trace(go.Scatter(x=[data.index[-1]], y=[fig.data[0]['y'][-1]-0.004], mode='markers', marker_symbol='triangle-up', marker_color='red', name='Latest Release: ' + f"{update:%m/%d/%y}" + f" ({data.index[-1]:%b})"))
+    # fig.add_vline(x=data.index[-1], line_width=3, line_dash="dash", line_color="green")
+    fig.update_layout(template='seaborn', 
+                      showlegend=True,
+                      title=dict(text=title, font=dict(size=25)),
+                      margin=dict(b=0,t=80,r=0,l=0),
+                      legend = dict(orientation='h',x=1, y=-0.15, xanchor='right', yanchor='top'), 
+                      # legend = dict(x=0.01, y=0.99, xanchor='left', yanchor='top'),
+                      xaxis=dict(fixedrange=True), 
+                      yaxis=dict(tickformat='.1%', fixedrange=True))
+    return fig, data
+
+  if 'core_yoy_fig_period' not in st.session_state: 
+    st.session_state['core_yoy_fig_period'] = 'Past 2 Years'
+  ##### Trigger function below #####
+  fig, data = core_yoy( dt.today()-timedelta(days=365*float(st.session_state['core_yoy_fig_period'][5])), dt.today() )
+  st.plotly_chart(fig, use_container_width=True)
+  st.radio('Display Period', ['Past 1 Year', 'Past 2 Years', 'Past 3 Years', 'Past 5 Years'], key='core_yoy_fig_period', horizontal=True)  
+
+with tab2:
+###################################### CPI MoM ######################################
+  def core_mom(start, end):
+    data = combine[['CPI Core Goods', 'CPI Core Service']].pct_change().dropna().loc[start:end]
+    update = updates['CPI']
+    title = 'Core Goods vs Core Service MoM'
+
+    fig = go.Figure()
+    fig.add_trace(go.Bar(x=data.index, y=data['CPI Core Goods'], name='Core Goods MoM'))
+    fig.add_trace(go.Bar(x=data.index, y=data['CPI Core Service'], name='Core Service MoM'))
+    # fig.add_trace(go.Scatter(x=[data.index[-1]], y=[fig.data[0]['y'][-1]-0.003], mode='markers', marker_symbol='triangle-up', marker_color='red', name='Latest Release: ' + f"{update:%m/%d/%y}" + f" ({data.index[-1]:%b})"))
+    # fig.add_vline(x=data.index[-1], line_width=3, line_dash="dash", line_color="green")
+    fig.update_layout(template='seaborn', 
+                      title=dict(text=title, 
+                      font=dict(size=25)), 
+                      margin=dict(b=0,t=80,r=0,l=0), 
+                      legend = dict(orientation='h',x=1, y=-0.15, xanchor='right', yanchor='top'),
+                      xaxis=dict(fixedrange=True),
+                      yaxis=dict(tickformat='.1%', fixedrange=True))
+    return fig, data
+
+  if 'core_mom_fig_period' not in st.session_state: 
+    st.session_state['core_mom_fig_period'] = 'Past 2 Years'
+  ##### Trigger function below #####
+  fig, data = core_mom( dt.today()-timedelta(days=365*float(st.session_state['core_mom_fig_period'][5])), dt.today() )
+  st.plotly_chart(fig, use_container_width=True)
+  st.radio('Display Period', ['Past 1 Year', 'Past 2 Years', 'Past 3 Years', 'Past 5 Years'], key='core_mom_fig_period', horizontal=True)  
+
+
+st.markdown('***')
+tab1, tab2, tab3, tab4 = st.tabs(['1','2','3', '4'])
+with tab1:
+###################################### CPI service yoy ######################################
+  def core_service(start, end):
+    data = combine[['CPI Core Service']].pct_change(12).dropna().loc[start:end]
+    update = updates['CPI']
+    title = 'Core Service YoY'
+
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=data.index, y=data['CPI Core Service'], mode='lines+markers', name='Core Service MoM'))
+    fig.add_trace(go.Scatter(x=[data.index[-1]], y=[fig.data[0]['y'][-1]-0.003], mode='markers', marker_symbol='triangle-up', marker_color='red', name='Latest Release: ' + f"{update:%m/%d/%y}" + f" ({data.index[-1]:%b})"))
+    # fig.add_vline(x=data.index[-1], line_width=3, line_dash="dash", line_color="green")
+    fig.update_layout(template='seaborn', 
+                      title=dict(text=title, font=dict(size=25)), 
+                      margin=dict(b=0,t=80,r=0,l=0), 
+                      legend=dict(orientation='h',x=1, y=-0.15, xanchor='right', yanchor='top'),
+                      xaxis=dict(fixedrange=True),
+                      yaxis=dict(tickformat='.1%', fixedrange=True))
+    return fig, data
+
+  if 'core_service_fig_period' not in st.session_state: 
+    st.session_state['core_service_fig_period'] = 'Past 2 Years'
+  ##### Trigger function below #####
+  fig, data = core_service( dt.today()-timedelta(days=365*float(st.session_state['core_service_fig_period'][5])), dt.today() )
+  st.plotly_chart(fig, use_container_width=True)
+  st.radio('Display Period', ['Past 1 Year', 'Past 2 Years', 'Past 3 Years', 'Past 5 Years'], key='core_service_fig_period', horizontal=True)  
+
+with tab2:
+###################################### CPI shelter yoy ######################################
+  def shelter_yoy(start, end):
+    data = combine[['Shelter']].pct_change(12).dropna().loc[start:end]
+    update = updates['CPI']
+    title = 'Shelter YoY'
+
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=data.index, y=data['Shelter'], mode='lines+markers', name='Shelter YoY'))
+    fig.add_trace(go.Scatter(x=[data.index[-1]], y=[fig.data[0]['y'][-1]-0.003], mode='markers', marker_symbol='triangle-up', marker_color='red', name='Latest Release: ' + f"{update:%m/%d/%y}" + f" ({data.index[-1]:%b})"))
+    # fig.add_vline(x=data.index[-1], line_width=3, line_dash="dash", line_color="green")
+    fig.update_layout(template='seaborn', 
+                      title=dict(text=title, font=dict(size=25)), 
+                      margin=dict(b=0,t=80,r=0,l=0), 
+                      legend=dict(orientation='h',x=1, y=-0.15, xanchor='right', yanchor='top'),
+                      xaxis=dict(fixedrange=True),
+                      yaxis=dict(tickformat='.1%', fixedrange=True))
+    return fig, data
+
+  if 'shelter_yoy_fig_period' not in st.session_state: 
+    st.session_state['shelter_yoy_fig_period'] = 'Past 2 Years'
+  ##### Trigger function below #####
+  fig, data = shelter_yoy( dt.today()-timedelta(days=365*float(st.session_state['shelter_yoy_fig_period'][5])), dt.today() )
+  st.plotly_chart(fig, use_container_width=True)
+  st.radio('Display Period', ['Past 1 Year', 'Past 2 Years', 'Past 3 Years', 'Past 5 Years'], key='shelter_yoy_fig_period', horizontal=True)  
+
+with tab3:
+###################################### CPI shelter mom ######################################
+  def shelter_yoy(start, end):
+    data = combine[['Shelter']].pct_change().dropna().loc[start:end]
+    update = updates['CPI']
+    title = 'Shelter MoM'
+
+    fig = go.Figure()
+    fig.add_trace(go.Bar(x=data.index, y=data['Shelter'], name='Shelter MoM')) #mode='lines+markers'
+    fig.add_trace(go.Scatter(x=[data.index[-1]], y=[fig.data[0]['y'][-1]-0.003], mode='markers', marker_symbol='triangle-up', marker_color='red', name='Latest Release: ' + f"{update:%m/%d/%y}" + f" ({data.index[-1]:%b})"))
+    # fig.add_vline(x=data.index[-1], line_width=3, line_dash="dash", line_color="green")
+    fig.update_layout(template='seaborn', 
+                      title=dict(text=title, font=dict(size=25)), 
+                      margin=dict(b=0,t=80,r=0,l=0), 
+                      legend=dict(orientation='h',x=1, y=-0.15, xanchor='right', yanchor='top'),
+                      xaxis=dict(fixedrange=True),
+                      yaxis=dict(tickformat='.1%', fixedrange=True))
+    return fig, data
+
+  if 'shelter_mom_fig_period' not in st.session_state: 
+    st.session_state['shelter_mom_fig_period'] = 'Past 2 Years'
+  ##### Trigger function below #####
+  fig, data = shelter_yoy( dt.today()-timedelta(days=365*float(st.session_state['shelter_mom_fig_period'][5])), dt.today() )
+  st.plotly_chart(fig, use_container_width=True)
+  st.radio('Display Period', ['Past 1 Year', 'Past 2 Years', 'Past 3 Years', 'Past 5 Years'], key='shelter_mom_fig_period', horizontal=True)  
+
+with tab4:
+  def service_others_mom(start, end):
+    data = combine[['Medical Care', 'Transportation', 'Education and Communication', 'Recreation']].pct_change().dropna().loc[start:end]
+    update = updates['CPI']
+    title = 'Other Core Service MoM'
+
+    fig = make_subplots(rows=2, cols=2, subplot_titles=('Medical Care MoM', 'Transportation MoM', 'Education and Communication MoM', 'Recreation MoM'))
+    fig.add_trace(go.Bar(x=data.index, y=data['Medical Care'], name='Medical Care MoM'), row=1, col=1)
+    fig.add_trace(go.Bar(x=data.index, y=data['Transportation'], name='Transportation MoM'), row=1, col=2)
+    fig.add_trace(go.Bar(x=data.index, y=data['Education and Communication'], name='Education and Communication MoM'), row=2, col=1)
+    fig.add_trace(go.Bar(x=data.index, y=data['Recreation'], name='Recreation MoM'), row=2, col=2)
+    # fig.add_trace(go.Scatter(x=[data.index[-1]], y=[fig.data[0]['y'][-1]-0.002], mode='markers', marker_symbol='triangle-up', marker_color='red', name='Latest Release: ' + f"{update:%m/%d/%y}" + f" ({data.index[-1]:%b})"))
+    # fig.add_vline(x=data.index[-1], line_width=3, line_dash="dash", line_color="green")
+    fig.update_layout(template='seaborn', 
+                      title=dict(text=title, font=dict(size=25)),
+                      margin=dict(b=0,t=80,r=0,l=0),
+                      legend = dict(orientation='h',x=1, y=-0.15, xanchor='right', yanchor='top'))
+
+    fig.update_yaxes(tickformat='.1%',fixedrange=True)
+    fig.update_xaxes(fixedrange=True)
+    
+    return fig, data
+
+  if 'service_others_mom_fig_period' not in st.session_state: 
+    st.session_state['service_others_mom_fig_period'] = 'Past 2 Years'
+  ##### Trigger function below #####
+  fig, data = service_others_mom( dt.today()-timedelta(days=365*float(st.session_state['service_others_mom_fig_period'][5])), dt.today() )
+  st.plotly_chart(fig, use_container_width=True)
+  st.radio('Display Period', ['Past 1 Year', 'Past 2 Years', 'Past 3 Years', 'Past 5 Years'], key='service_others_mom_fig_period', horizontal=True) # label_visibility='collapsed' 
